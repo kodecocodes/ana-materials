38e0ce3d7fc15ac65e2b8f06f6f3fb7e083f7980
Godday Okoduwa <goddayokos@gmail.com>
2021-08-25 12:58:13 +0100

    Wrote Unit test for Movie class

---
+++ 01-introduction/projects/starter/app/src/main/java/com/raywenderlich/wewatch/test/MovieTest.kt
@@ -0,0 +1,31 @@
+package com.raywenderlich.wewatch.test
+
+import com.raywenderlich.wewatch.model.Movie
+import org.junit.Assert.*
+import org.junit.Test
+
+class MovieTest {
+    @Test
+    fun testGetReleaseYearFromStringFormmattedDate() {
+        val movie = Movie(title = "Finding Nemo", releaseDate = "2003-05-30")
+        assertEquals("2003", movie.getReleaseYearFromDate())
+    }
+
+    @Test
+    fun testGetReleaseYearFromYear() {
+        val movie = Movie(title = "FindingNemo", releaseDate = "2003")
+        assertEquals("2003", movie.getReleaseYearFromDate())
+    }
+
+    @Test
+    fun testGetReleaseYearFromDateEdgeCaseEmpty() {
+        val movie = Movie(title = "FindingNemo", releaseDate = "")
+        assertEquals("", movie.getReleaseYearFromDate())
+    }
+
+    @Test
+    fun testGetReleaseYearFromDateEdgeCaseNull() {
+        val movie = Movie(title = "FindingNemo")
+        assertEquals(null, movie.getReleaseYearFromDate())
+    }
+}
